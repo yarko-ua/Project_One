a44fd29dab04df89fffc923b6c470cc7635f2ca3
Yaroslav <ssilvias13@gmail.com>
2020-02-16 18:22:29 +0200

    change color palitra

--- src/components/Main/Filter/Filter.js
+++ src/components/Main/Filter/Filter.js
@@ -25,6 +25,14 @@ class Filter extends Component {
                 <Input title="Gender" type="radio" name="gender"
                     options={["Male", "Female"]}
                 />
+                <Input title="Brand" type="checkbox" name="brand"
+                    options={["Burton", "LibTech", "Ride", "Capita", "Union"]}
+
+                />
+                <Input title="Size" type="checkbox" name="size"
+                    options={["XS", "S", "M", "L", "XL", "152cm", "155cm", "160cm", "7.0", "7.5"]}
+
+                />
             </>
         )
     }
