3c14a4fcd5a1b5a1cd5c75b07376fa6962033ed8
Yaroslav <ssilvias13@gmail.com>
2020-02-18 17:53:44 +0200

    ver. 0.1

---
+++ build/precache-manifest.40832300f621d59b6d974166dbf13f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce29a442370beaa5d5f5dc0d06d3b906",
+    "url": "/Project_One/build/index.html"
+  },
+  {
+    "revision": "98aade71e88fe9288568",
+    "url": "/Project_One/build/static/css/main.15ad9d95.chunk.css"
+  },
+  {
+    "revision": "744d9037650ba6b626fa",
+    "url": "/Project_One/build/static/js/2.673cfb58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Project_One/build/static/js/2.673cfb58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98aade71e88fe9288568",
+    "url": "/Project_One/build/static/js/main.ac772073.chunk.js"
+  },
+  {
+    "revision": "ea2f59e2b664ba9b359d",
+    "url": "/Project_One/build/static/js/runtime-main.dfc2be51.js"
+  },
+  {
+    "revision": "94570fee461b9f2dc22601b85e5de4c7",
+    "url": "/Project_One/build/static/media/backgr1.94570fee.jpg"
+  }
+]);
